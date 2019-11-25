f99a4656a0623c9aae118618f05722a2c499a730
Ishan Subedi <ikushum@gmail.com>
2019-11-25 23:44:06 +0545

    implement logout

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,34 @@
           Kakshya
         </v-btn>
       </v-toolbar-title>
+      <v-spacer />
+      <v-menu
+        left
+        bottom
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn 
+            icon
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>View Profile</v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            @click="$auth.logout()"
+          >
+            <v-list-item-title>
+              Logout
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>      
     </v-app-bar>
     <nuxt class="mt-12" />
   </v-app>
