0d2337b75d7516ce4d1701561d5e54ae7186e7bd
Ishan Subedi <ikushum@gmail.com>
2019-11-26 02:54:53 +0545

    create about page

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row
+      justify="center"
+    >
+      <v-col
+        cols="8"
+        class="text-center"
+      >
+        <v-icon
+          size="150"
+          v-text="'mdi-school'"
+        />      
+        <h1> About Kakshya </h1>
+        <h3 class="subtitle-1 my-6">
+          <b>Kakshya</b> is a web based application that provides a virtual classroom experience in 
+          real time where teachers and student get to interact with one another live, to make both
+          learning and teaching experience more efficient.
+        </h3>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
